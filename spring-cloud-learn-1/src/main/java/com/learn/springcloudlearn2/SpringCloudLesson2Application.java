c24390fbda6b9fabff4d155ba9b5493a0e96221e
YUN <yunpeng73542@icloud.com>
2018-11-12 22:45:36 +0800

    learn2

---
+++ spring-cloud-learn-1/src/main/java/com/learn/springcloudlearn2/SpringCloudLesson2Application.java
@@ -0,0 +1,16 @@
+package com.learn.springcloudlearn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by JAVA on 2018/11/12.
+ */
+@SpringBootApplication
+public class SpringCloudLesson2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudLesson2Application.class, args);
+    }
+
+}
