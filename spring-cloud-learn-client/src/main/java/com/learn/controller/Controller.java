2946161611dbb06687bfd81564f6cc19c63dcf0b
suantou <suantou@helijia.com>
2019-01-03 14:46:10 +0800

    spring cloud client

---
+++ spring-cloud-learn-client/src/main/java/com/learn/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.learn.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello world from port " + port;
+    }
+}
