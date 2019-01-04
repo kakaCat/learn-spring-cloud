5f64986603081409a93e19c3dfc44727e53c2c0c
suantou <suantou@helijia.com>
2019-01-04 13:50:29 +0800

    spring cloud client

--- spring-cloud-learn-client/src/main/java/com/learn/EurekaClientApplication.java
+++ spring-cloud-learn-client/src/main/java/com/learn/EurekaClientApplication.java
@@ -4,11 +4,21 @@ package com.learn;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class EurekaClientApplication {
+
+    //@Bean 应用在方法上，用来将方法返回值设为为bean
+    @Bean
+    @LoadBalanced  //@LoadBalanced实现负载均衡
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
--- spring-cloud-learn-client/src/main/java/com/learn/controller/Controller.java
+++ spring-cloud-learn-client/src/main/java/com/learn/controller/Controller.java
@@ -1,8 +1,10 @@
 package com.learn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Controller {
@@ -14,4 +16,17 @@ public class Controller {
     public String home() {
         return "hello world from port " + port;
     }
+
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/hello-consumer")
+    public String helloConsumer() {
+        //调用hello-service服务，注意这里用的是服务名，而不是具体的ip+port
+        restTemplate.getForObject("http://hello-service/hello", String.class);
+        return "hello consumer finish !!!";
+    }
+
 }
