8a1554f94682a10dcf07cef6757a71714d5c84d1
yun <yunpeng@huaxiafinance.com>
2019-10-28 15:13:01 +0800

    temp

--- learn-spring/learn-spring-module/src/main/java/learn/base/loadbalance/InetAddressDome.java
+++ learn-spring/learn-spring-module/src/main/java/learn/base/loadbalance/InetAddressDome.java
@@ -1,17 +1,20 @@
 package learn.base.loadbalance;
 
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.LinkedHashMap;
 import java.util.Random;
 import java.util.stream.Stream;
 
 public class InetAddressDome {
 
     public static void main(String[] args) throws UnknownHostException {
-        Stream.of(InetAddress.getAllByName("www.baidu.com")).forEach(System.out::println);
+        Stream.of(InetAddress.getAllByName("www.taobao.com")).forEach(System.out::println);
     }
 
     public void random (){
+
 
         Random random = new Random();
         int size =2;
